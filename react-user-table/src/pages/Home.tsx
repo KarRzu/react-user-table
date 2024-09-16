0d600d9af2774f7905da62a9f8108da8e39b0d0e
Karolina <karola.rzucidlo14@gmail.com>
2024-09-16 20:34:27 +0200

    chore: add react router dom

--- react-user-table/src/pages/Home.tsx
+++ react-user-table/src/pages/Home.tsx
@@ -1,8 +1,10 @@
+import { BrowserRouter } from "react-router-dom";
 import { Table } from "../components/shared/Table";
 
 export function Home() {
   return (
     <>
+      <BrowserRouter></BrowserRouter>
       <Table />
     </>
   );
